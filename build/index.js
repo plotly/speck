e8ee85f2980523d4611ea74cc300c586c26ff8bb
Daniel Mejia <denphi@denphi.com>
2020-12-16 11:27:34 -0500

    Fixing the demo (index.htm)

--- build/index.js
+++ build/index.js
@@ -3,6 +3,7 @@ const speckSystem = require('../src/system.js');
 const speckView = require('../src/view.js');
 const speckInteractions = require('../src/interactions.js');
 const speckPresetViews = require('../src/presets.js');
+const speckMain = require('../src/main.js');
 
 module.exports = {
     speckRenderer,
