1525d4854c7bdde96c43bf251c81eae31aa9c9b7
Daniel Mejia <denphi@denphi.com>
2021-01-08 12:18:38 -0500

    prerelease 0.0.4

--- widget/ipyspeck/ipyspeck/_version.py
+++ widget/ipyspeck/ipyspeck/_version.py
@@ -5,4 +5,4 @@ version_info = (0, 4, 0)
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
 
 # Module version accessible using ipyspeck.__version__
-__version__ = '%s.%s.%s'%(version_info[0], version_info[1], version_info[2]))
+__version__ = '%s.%s.%s'%(version_info[0], version_info[1], version_info[2])
