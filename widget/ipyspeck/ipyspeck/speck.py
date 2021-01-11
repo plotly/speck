0089282fcb762d678ca3ec130d1e2566b59b57c1
Daniel Mejia <denphi@denphi.com>
2021-01-11 17:34:42 -0500

    V0.6.0

--- widget/ipyspeck/ipyspeck/speck.py
+++ widget/ipyspeck/ipyspeck/speck.py
@@ -20,9 +20,9 @@ class Speck(widgets.DOMWidget):
     _model_module = Unicode('ipyspeck').tag(sync=True)
 
     # Version of the front-end module containing widget view
-    _view_module_version = Unicode('^0.5.0').tag(sync=True)
+    _view_module_version = Unicode('^0.6.0').tag(sync=True)
     # Version of the front-end module containing widget model
-    _model_module_version = Unicode('^0.5.0').tag(sync=True)
+    _model_module_version = Unicode('^0.6.0').tag(sync=True)
 
     # Widget specific property.
     # Widget properties are defined as traitlets. Any property tagged with `sync=True`
