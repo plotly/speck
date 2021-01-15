d7b769048e1d347dc0917ad34cc9011f1e91c98f
Daniel Mejia <denphi@denphi.com>
2021-01-15 13:35:35 -0500

    Adding Doc + data validation

--- widget/ipyspeck/ipyspeck/speck.py
+++ widget/ipyspeck/ipyspeck/speck.py
@@ -1,34 +1,80 @@
 import ipywidgets as widgets
-from traitlets import Unicode, Bool, Float
+from traitlets import Unicode, Bool, Float, validate, TraitError
 
-# See js/lib/example.js for the frontend counterpart to this file.
 
 @widgets.register
 class Speck(widgets.DOMWidget):
-    """An example widget."""
+    """
+    A class used to represent an Speck molecule
 
-    # Name of the widget view class in front-end
+    ...
+
+    Attributes
+    ----------
+    _view_name : str
+        (DOMWidget) Name of the widget view class in front-end
+    _model_name :dofStrength
+        Name of the widget model class in front-end
+    _view_module : str
+        (DOMWidget) Name of the front-end module containing widget view
+    _model_module : str
+        (DOMWidget) Name of the front-end module containing widget model
+    _view_module_version : str
+        (DOMWidget) Version of the front-end module containing widget view
+    _model_module_version : str
+        (DOMWidget) Version of the front-end module containing widget model
+    data : str
+        xyz model, default(True)
+    bonds : bool
+        Enable visualizations of bonds?, default(True)
+    atomScale : float
+        Atom radius, size of spheres, default(0.24)
+    relativeAtomScale : float
+        Relative atom radius, default(0.64)
+    bondScale : float
+        bonds size, size of the tubes connecting atoms, default(0.5)
+    brightness : float
+        brightness, default(0.5)
+    outline : float
+        Outline strength, default(0.0)
+    spf : float
+        Samples per frame, default(32)
+    bondThreshold : float
+        Bonding radius, defines the max distance for atoms to be connected,
+        default(1.2)
+    bondShade : float
+        bonds shade, default(0.5)
+    atomShade : float
+        Atoms shade, default(0.5)
+    dofStrength : float
+        Depth of field strength, default(0.0)
+    dofPosition : float
+        Depth of field position, default(0.5)
+
+    Methods
+    -------
+    frontview()
+        Change to front camera / view
+    topview()
+        Change to top camera / view
+    rightview()
+        Change to right camera / view
+    setAtomColor(atom, color)
+        Set the 'color' of all atoms with the name 'atom'
+    setAtomsColor(atoms)
+        Set multiple colors to atoms
+    setColorSchema(schema)
+        Set the color Schema
+    switchColorSchema()
+        Switch to the next available schema
+    """
+
     _view_name = Unicode('SpeckView').tag(sync=True)
-
-    # Name of the widget model class in front-end
     _model_name = Unicode('SpeckModel').tag(sync=True)
-
-    # Name of the front-end module containing widget view
     _view_module = Unicode('ipyspeck').tag(sync=True)
-
-    # Name of the front-end module containing widget model
     _model_module = Unicode('ipyspeck').tag(sync=True)
-
-    # Version of the front-end module containing widget view
     _view_module_version = Unicode('^0.6.0').tag(sync=True)
-    # Version of the front-end module containing widget model
     _model_module_version = Unicode('^0.6.0').tag(sync=True)
-
-    # Widget specific property.
-    # Widget properties are defined as traitlets. Any property tagged with `sync=True`
-    # is automatically synced to the frontend *any* time it changes in Python.
-    # It is synced back to Python from the frontend *any* time the model is touched.
-
     data = Unicode('').tag(sync=True)
     bonds = Bool(True).tag(sync=True)
     atomScale = Float(0.24).tag(sync=True)
@@ -43,23 +89,68 @@ class Speck(widgets.DOMWidget):
     dofStrength = Float(0.0).tag(sync=True)
     dofPosition = Float(0.5).tag(sync=True)
 
+    @validate('data')
+    def _valid_data(self, proposal):
+        if False:
+            raise TraitError('Invalid XYZ')
+        return proposal['value']
+
     def frontview(self):
-        self.send({"do":"frontView"})
+        """Rotates the molecule model to visualize the front view"""
+        self.send({"do": "frontView"})
 
     def topview(self):
-        self.send({"do":"topView"})
+        """Rotates the molecule model to visualize the top view"""
+        self.send({"do": "topView"})
 
     def rightview(self):
-        self.send({"do":"rightView"})
+        """Rotates the molecule model to visualize the rigth view"""
+        self.send({"do": "rightView"})
 
     def setAtomColor(self, atom, color):
-        self.setAtomsColor({ atom : color})
+        """Set the color of atom types
+
+        Parameters
+        ----------
+        atom : str
+            Atom Name
+        color : list
+            A list with 3 rgb normalized values [0 - 1]
+        """
+        self.setAtomsColor({atom: color})
 
     def setAtomsColor(self, atoms):
-        self.send({"do":"changeAtomsColor","atoms":atoms})
+        """Set the color of multiple atom types
+
+        Parameters
+        ----------
+        atoms : dict
+            A dictionary with tuples key as str and value rgb normalized values
+            [0 - 1]
+        """
+        for atom, rgb in atoms.items():
+            if not isinstance(atom, str):
+                raise Exception(
+                    "atom names should be str,  '" + type(atom) + "' passed"
+                )
+            if len(rgb) != 3:
+                raise Exception("RGB values should contain exactly 3 elements")
+            for i in range(3):
+                if rgb[i] < 0 or rgb[i] > 1:
+                    raise Exception("RGB values should be [0 - 1] range")
+        self.send({"do": "changeAtomsColor", "atoms": atoms})
 
     def setColorSchema(self, schema):
-        self.send({"do":"changeColorSchema","schema":schema})
+        """Set the color schema used by Speck, overwrites any custom change in
+           atom color
+
+        Parameters
+        ----------
+        schema : str
+            name of the schema/palette to use
+        """
+        self.send({"do": "changeColorSchema", "schema": schema})
 
     def switchColorSchema(self):
-        self.send({"do":"changeColorSchema"})
+        """Switch to the next available color schema """
+        self.send({"do": "changeColorSchema"})
