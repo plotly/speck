aec2d1e606036dced9424fe63bb2388e48fd5c01
Daniel Mejia <denphi@denphi.com>
2022-05-20 19:26:15 -0400

    0.6.1 release

--- widget/ipyspeck/setup.py
+++ widget/ipyspeck/setup.py
@@ -25,21 +25,29 @@ name = 'ipyspeck'
 version = get_version(pjoin(name, '_version.py'))
 
 js_dir = pjoin(here, 'js')
+js_dir2 = pjoin(here, 'ipyspeck', 'stspeck', 'frontend')
 
 # Representative files that should exist after a successful build
 jstargets = [
     pjoin(js_dir, 'dist', 'index.js'),
 ]
-
+jstargets2 = [
+    pjoin(js_dir2, 'build', 'static'),
+]
 data_files_spec = [
     ('share/jupyter/nbextensions/ipyspeck', 'ipyspeck/static', '*.*'),
     ('etc/jupyter/nbconfig/notebook.d', '.', 'ipyspeck.json'),
+    ('share/streamlit/component/stspeck', 'ipyspeck/stspeck/frontend/build', '*.*'),
+    ('share/streamlit/component/stspeck/static', 'ipyspeck/stspeck/frontend/build/static', '*.*'),
+    ('share/streamlit/component/stspeck/static/js', 'ipyspeck/stspeck/frontend/build/static/js', '*.*'),
 ]
 
 cmdclass = create_cmdclass('jsdeps', data_files_spec=data_files_spec)
 cmdclass['jsdeps'] = combine_commands(
     install_npm(js_dir, build_cmd='build'), ensure_targets(jstargets),
+    install_npm(js_dir2, build_cmd='build'), ensure_targets(jstargets2),
 )
+
 
 LONG_DESCRIPTION = ""
 with open("README.md", "r") as fh:
@@ -54,7 +62,7 @@ setup_args = dict(
     include_package_data=True,
     install_requires=[
         'ipywidgets>=7.0.0',
-    ],
+        'jupyter_packaging>=0.3.0'    ],
     packages=find_packages(),
     zip_safe=False,
     cmdclass=cmdclass,
